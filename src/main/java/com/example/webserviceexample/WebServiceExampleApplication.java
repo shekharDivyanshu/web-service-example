bcf63fdbf41aa6671faeba4086c9519031d468e0
divyanshu Shekhar <dshekhar@divyanshus-MacBook-Pro.local>
2019-12-01 13:32:23 -0500

    initial commit

---
+++ src/main/java/com/example/webserviceexample/WebServiceExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.webserviceexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebServiceExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebServiceExampleApplication.class, args);
+	}
+
+}
